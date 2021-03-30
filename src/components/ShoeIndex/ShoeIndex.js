7da5bd99a59d813864e77212728e8170addbfef8
Chris Finn <chris@finn.com>
2021-03-29 22:36:54 -0300

    [3] Implement Shell.

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -16,6 +16,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Header>
           <Title>Running</Title>
           <Select
+            style={{ flexBasis: 300 }}
             label="Sort"
             value={sortId}
             onChange={(ev) => setSortId(ev.target.value)}
@@ -42,13 +43,27 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  flex: 1;
+  /* Is there a way to align to right column baseline? */
+  padding-top: 14px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  flex: 4;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
